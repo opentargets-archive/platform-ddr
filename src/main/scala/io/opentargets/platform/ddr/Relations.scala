31e92139f394520201a8112b016f6d9642755faa
Miguel C <mkarmona@gmail.com>
2019-01-06 23:23:09 +0000

    feat: use a grouped list of targets and diseases

--- src/main/scala/io/opentargets/platform/ddr/Relations.scala
+++ src/main/scala/io/opentargets/platform/ddr/Relations.scala
@@ -5,20 +5,29 @@ import io.opentargets.platform.ddr.algorithms.SimilarityIndex._
 import io.opentargets.platform.ddr.algorithms.SimilarityIndex
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-
 object Relations extends LazyLogging {
-  def apply(df: DataFrame, numSynonyms: Int)(implicit ss: SparkSession): Option[DataFrame] = {
+  def apply(df: DataFrame, numSynonyms: Int)(
+    implicit ss: SparkSession): Option[DataFrame] = {
     val params = SimilarityIndexParams()
     val algo = new SimilarityIndex(params)
 
     val diseasesModel =
-      algo.fit(df, groupBy = "target_id", aggBy = Seq("disease_id", "disease_label", "score", "count"))
+      algo.fit(df,
+        groupBy = "target_id",
+        aggBy = Seq("disease_id", "disease_label", "score", "count"))
 
     val targetsModel =
-      algo.fit(df, groupBy = "disease_id", aggBy = Seq("target_id", "target_symbol", "score", "count"))
+      algo.fit(df,
+        groupBy = "disease_id",
+        aggBy = Seq("target_id", "target_symbol", "score", "count"))
 
-    val dsyns = diseasesModel.map(_.findSynonyms(numSynonyms)(df, "disease_id", "disease_synonyms"))
-    val tsyns = targetsModel.map(_.findSynonyms(numSynonyms)(df, "target_id", "target_synonyms"))
+    val targetsDF = df.select("target_id").distinct().toDF("target_id")
+    val diseasesDF = df.select("disease_id").distinct().toDF("disease_id")
+
+    val dsyns = diseasesModel.map(
+      _.findSynonyms(numSynonyms)(diseasesDF, "disease_id", "disease_synonyms"))
+    val tsyns = targetsModel.map(
+      _.findSynonyms(numSynonyms)(targetsDF, "target_id", "target_synonyms"))
 
     val dfs = List(dsyns, tsyns).filter(_.isDefined).map(_.get)
     logger.debug(s"computed synonym dataframes count ${dfs.length}")
